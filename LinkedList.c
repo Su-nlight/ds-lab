909439c72b40b5e481aebdfe05a537edfa522a30
Sumit Sharma <84061534+Su-nlight@users.noreply.github.com>
2024-10-01 10:48:31 +0530

    Update LinkedList.c

--- LinkedList.c
+++ LinkedList.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include<alloc.h>
+#include<stdlib.h>
 
 struct node
 {
@@ -8,7 +8,37 @@ struct node
 };
 struct node *start=NULL;
 void create();
-void traverse();
+void traverse();#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+        int info;
+        struct node *link;
+};
+struct node* start=NULL;
+
+void create(){
+    struct node* ptr;
+    ptr=(struct node*) malloc(sizeof(struct node));
+    printf("Enter the information to be stored in node: ");
+    scanf("%d",&ptr->info);
+    ptr->link=NULL;
+    start=ptr;
+}
+
+void traverse(){
+    struct node* ptr;
+    int nodeno=0;
+    for(ptr=start; ptr!=NULL; ptr=ptr->link,nodeno++){
+        printf("Node no.%d has info = %d", nodeno, ptr->info);
+    }
+}
+int main(){
+    create();
+    traverse();
+    return 0;
+}
 void main(){
     void create();
     void traverse();
