1a14e28776bcd330a23e3c6c254c30d0a57993c9
Sumit Sharma <84061534+Su-nlight@users.noreply.github.com>
2024-10-08 10:13:21 +0530

    Update LinkedList.c

--- LinkedList.c
+++ LinkedList.c
@@ -6,57 +6,103 @@ struct node
         int info;
         struct node *link;
 };
-struct node *start=NULL;
-void create();
-void traverse();#include<stdio.h>
-#include<stdlib.h>
-
-struct node
-{
-        int info;
-        struct node *link;
-};
 struct node* start=NULL;
 
-void create(){
+struct node* create_node(int data){
     struct node* ptr;
     ptr=(struct node*) malloc(sizeof(struct node));
-    printf("Enter the information to be stored in node: ");
-    scanf("%d",&ptr->info);
+    if(ptr==NULL){
+        printf("Overflow");
+        return NULL;
+    }
     ptr->link=NULL;
+    ptr->info=data;
+    return ptr;
+}
+
+void create(){
+    int data;
+    struct node* ptr;
+    printf("Enter the information to be stored in node: ");
+    scanf("%d",&data);
+    ptr=create_node(data);
     start=ptr;
 }
 
+void insert_Begn(){
+    struct node* new;
+    int data;
+    printf("Enter the information to be stored in node(at begenning): ");
+    scanf("%d",&data);
+    new = create_node(data);
+    if(new==NULL){ return;}
+    new->link=start;
+    start=new;
+}
+void insert_End(){
+    struct node* new;
+    int data;
+    printf("Enter the information to be stored in node(at End): ");
+    scanf("%d",&data);
+    new = create_node(data);
+    if(new==NULL){ return;}
+    if(start==NULL){
+        start=new;
+        return;
+    }
+    //else traverse to last 
+    struct node* temp=start;
+    while(temp->link!=NULL){
+        temp=temp->link;
+    }
+    temp->link=new;
+    return;
+}
+void delete_Begn(){
+    struct node* tobedel=start;
+    if(tobedel==NULL){
+        printf("List is Empty.");
+        return;
+    }
+    else{
+        start=tobedel->link;
+        free(tobedel);
+    }
+    
+}
+
+void delete_End(){
+    struct node* temp=start;
+    if(temp==NULL){
+        printf("List is Empty.");
+        return;
+    }
+    while(temp->link->link!=NULL){
+        temp=temp->link;
+    }
+    if(temp->link->link==NULL){
+        temp->link=NULL;
+        free(temp->link);
+        return;
+    }
+    printf("Error");
+}
 void traverse(){
     struct node* ptr;
     int nodeno=0;
     for(ptr=start; ptr!=NULL; ptr=ptr->link,nodeno++){
-        printf("Node no.%d has info = %d", nodeno, ptr->info);
+        printf("Node no.%d has info = %d \n", nodeno+1, ptr->info);
     }
 }
 int main(){
     create();
+    insert_Begn();
+    insert_End();
+    insert_Begn();
+    insert_End();
+    traverse();
+    delete_Begn();
+    delete_End();
     traverse();
     return 0;
-}
-void main(){
-    void create();
-    void traverse();
-}
-
-void create(){
-    struct node *ptr;
-    ptr=(struct node*)malloc(sizeof(struct node));
-    printf("Enter the information to be stored in node: ");
-    scanf("%d",&ptr->info);
-    ptr->link=NULL;
-    start=ptr;
-}
-
-void traverse(){
-    struct node *ptr;
-    int nodeno=0;
-    for(ptr=start; ptr!=NULL; ptr=ptr->link,nodeno++){
-        print("Node no.%d has info = %d", nodeno, ptr->info);
-    }
 }
